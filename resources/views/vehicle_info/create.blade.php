ac7e1c01bab11a83a2d6e5ee74239a6629af22ae
nikeshjaintms <nikeshjaintms@gmail.com>
2025-01-21 09:38:12 +0530

    first commit

--- resources/views/vehicle_info/create.blade.php
+++ resources/views/vehicle_info/create.blade.php
@@ -1,74 +1,59 @@
 @extends('layouts.app')
 
 @section('content-page')
-
-<div class="container">
-    <div class="page-inner">
-      <div class="page-header">
-        <h3 class="fw-bold mb-3">Vehicle Information</h3>
-        <ul class="breadcrumbs mb-3">
-          <li class="nav-home">
-            <a href="#">
-              <i class="icon-home"></i>
-            </a>
-          </li>
-          <li class="separator">
-            <i class="icon-arrow-right"></i>
-          </li>
-          <li class="nav-item">
-            <a href="#">Vehicle Information</a>
-          </li>
-          <li class="separator">
-            <i class="icon-arrow-right"></i>
-          </li>
-          <li class="nav-item">
-            <a href="#">Add</a>
-          </li>
-        </ul>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-header">
-              <div class="card-title">Form Elements</div>
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h3 class="fw-bold mb-3">Vehicle Information</h3>
+                <ul class="breadcrumbs mb-3">
+                    <li class="nav-home">
+                        <a href="#">
+                            <i class="icon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="icon-arrow-right"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Vehicle Information</a>
+                    </li>
+                    <li class="separator">
+                        <i class="icon-arrow-right"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Add</a>
+                    </li>
+                </ul>
             </div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-md-6 col-lg-4">
-                  <div class="form-group">
-                    <label for="email2">Email Address</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="email2"
-                      placeholder="Enter Email"
-                    />
-                    <small id="emailHelp2" class="form-text text-muted"
-                      >We'll never share your email with anyone
-                      else.</small
-                    >
-                  </div>
-                  <div class="form-group">
-                    <label for="password">Password</label>
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="password"
-                      placeholder="Password"
-                    />
-                  </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title">Add Vehicle Information </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 col-lg-4">
+                                    <div class="form-group">
+                                        <label for="email2">Vehicle No</label>
+                                        <input type="text" class="form-control" id="email2" placeholder="GJ 16 XX 0000" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" id="password" placeholder="Password" />
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div class="card-action">
+                            <button class="btn btn-success">Submit</button>
+                            <button class="btn btn-danger">Cancel</button>
+                        </div>
+                    </div>
                 </div>
-
-
-              </div>
             </div>
-            <div class="card-action">
-              <button class="btn btn-success">Submit</button>
-              <button class="btn btn-danger">Cancel</button>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 @endsection
