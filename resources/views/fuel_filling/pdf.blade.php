fa83aa4631069e419e6f9bc54b7c797e36d4d54c
nikeshjaintms <nikeshjaintms@gmail.com>
2025-01-31 17:35:00 +0530

    pdf added

--- resources/views/fuel_filling/pdf.blade.php
+++ resources/views/fuel_filling/pdf.blade.php
@@ -8,38 +8,43 @@
 </head>
 <body>
     <h2> </h2>
-    <table style="border-collapse: collapse; width: 100%;">
-        <tr>
-            <th>ID</th>
-            <th>Vehicle</th>
-            <th>Driver</th>
-            <th>Customer</th>
-            <th>Filling Date</th>
-            <th>Quantity</th>
-            <th>Kilometers</th>
-            <th>Average (Fuel Consupmation)</th>
-            <th>Average(Claim by Company)</th>
-        </tr>
-        @if(count($data) > 0)
-        @foreach($data as $item)
-            <tr>
-                <td>{{$item->id }}</td>
-                <td>{{$item->vehicle_no }}</td>
-                <td>{{$item->driver_name }}</td>
-                <td>{{$item->customer_name }}</td>
-                <td>{{$item->filling_date }}</td>
-                <td>{{$item->quantity }}</td>
-                <td>{{$item->kilometers }}</td>
-                <td>{{$item->average_fuel_consumption }}</td>
-                <td>{{$item->average }}</td>
-                <>
-            </tr>
-        @endforeach
-        @else
+        @if($data->isEmpty())
         <p>Nothing to show</p>
-        @endif
+
+        @else
+        <table style="border-collapse: collapse; width: 100%;">
+            <tr>
+                <th>ID</th>
+                <th>Vehicle</th>
+                <th>Driver</th>
+                <th>Customer</th>
+                <th>Filling Date</th>
+                <th>Quantity</th>
+                <th>Kilometers</th>
+                <th>Average (Fuel Consupmation)</th>
+                <th>Average(Claim by Company)</th>
+            </tr>
+
+
+            @foreach($data as $item)
+                <tr>
+                    <td>{{$item->id }}</td>
+                    <td>{{$item->vehicle_no }}</td>
+                    <td>{{$item->driver_name }}</td>
+                    <td>{{$item->customer_name }}</td>
+                    <td>{{$item->filling_date }}</td>
+                    <td>{{$item->quantity }}</td>
+                    <td>{{$item->kilometers }}</td>
+                    <td>{{$item->average_fuel_consumption }}</td>
+                    <td>{{$item->average }}</td>
+                    <>
+                </tr>
+            @endforeach
+
+
         </tr>
     </table>
+@endif
 
 </body>
 </html>
